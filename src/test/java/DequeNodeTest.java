61e56bfca062b1a97954c22d32ed40caa26e0479
juanjose <juanjosetp02@uma.es>
2023-03-07 12:58:44 +0100

    necesito hacer pull

--- src/test/java/DequeNodeTest.java
+++ src/test/java/DequeNodeTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DequeNodeTest {
+
+
+    @Test
+    void isFirstFirstNode(){
+
+    }
+
+    @Test
+    void isFirstLastNode(){
+
+
+    }
+
+    @Test
+    void idMiddleNode(){
+
+    }
+
+
+
+
+
 }
